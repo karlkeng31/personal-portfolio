cd1831d15659de3f9631d3954d74e0f930760f3e
Karl Ken. G. <formycodingonly@gmail.com>
2024-01-26 23:03:07 +0800

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,128 @@
+import images from './images';
+
+export { images };
+
+export const abouts = [
+  {
+    title: 'Programmer',
+    img: images.programmer,
+    description:
+      "I'm, an aspiring programmer with a passion for coding and learning. While I may not have professional experience yet.",
+  },
+  {
+    title: 'Web Developer',
+    img: images.webDev,
+    description:
+      "I'm, a motivated web developer with a keen interest in crafting engaging and user-friendly online experiences.",
+  },
+];
+
+export const works = [
+  {
+    title: 'UI/UX Design',
+    img: images.programmer,
+    tags: 'UI/UX Design',
+    projectLink: 'https://github.com/karlken31/ims_project',
+    codeLink: 'https://github.com/karlken31/ims_project',
+    description:
+      "I'm, an aspiring programmer with a passion for coding and learning. While I may not have professional experience yet.",
+  },
+  {
+    title: 'React JS',
+    img: images.webDev,
+    tags: 'React JS',
+    projectLink: 'https://github.com/karlken31/ims_project',
+    description:
+      "I'm, a motivated web developer with a keen interest in crafting engaging and user-friendly online experiences.",
+  },
+  {
+    title: 'PHP',
+    img: images.webDev,
+    tags: 'PHP',
+    projectLink: 'https://github.com/karlken31/ims_project',
+    codeLink: 'https://github.com/karlken31/ims_project',
+    description:
+      "I'm, a motivated web developer with a keen interest in crafting engaging and user-friendly online experiences.",
+  },
+  {
+    title: 'Java',
+    img: images.webDev,
+    tags: 'Java',
+    projectLink: 'https://github.com/karlken31/ims_project',
+    codeLink: 'https://github.com/karlken31/ims_project',
+    description:
+      "I'm, a motivated web developer with a keen interest in crafting engaging and user-friendly online experiences.",
+  },
+  {
+    title: 'Python',
+    img: images.webDev,
+    tags: 'Python',
+    projectLink: 'https://github.com/karlken31/ims_project',
+    codeLink: 'https://github.com/karlken31/ims_project',
+    description:
+      "I'm, a motivated web developer with a keen interest in crafting engaging and user-friendly online experiences.",
+  },
+];
+
+export const skills = [
+  {
+    name: 'HTML',
+    icon: images.html,
+    bgColor: '#000',
+  },
+  {
+    name: 'CSS',
+    icon: images.css,
+    bgColor: '#000',
+  },
+  {
+    name: 'JavaScript',
+    icon: images.javascript,
+    bgColor: '#000',
+  },
+  {
+    name: 'Git',
+    icon: images.git,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+  {
+    name: 'Figma',
+    icon: images.figma,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+  {
+    name: 'Material UI',
+    icon: images.mu5,
+    bgColor: '#000',
+  },
+];
